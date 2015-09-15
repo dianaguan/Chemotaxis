1257455d7c21a9539d79b95e52e4e4355fb4e3fe
unknown <yiguan@697-334-Bilbo.sf.k12.internal>
2015-09-15 11:37:26 -0700

    Built it

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 //declare bacteria variables here 
 Bacteria [] colony;
- void setup()   
+ public void setup()   
  {     
    //initialize bacteria variables here 
    size(800,500);
@@ -10,7 +26,7 @@ Bacteria [] colony;
    		colony[i] = new Bacteria();
    }
  }   
- void draw()   
+ public void draw()   
  {    
    //move and show the bacteria   
    background(255);
@@ -31,7 +47,7 @@ Bacteria [] colony;
     bactColor2 = (int)(Math.random()*255);
     bactColor3 = (int)(Math.random()*255);
   }
-  void move()
+  public void move()
   {     
    //lots of java!   
    bactX = bactX + (int)(Math.random()*2)+1;
@@ -43,9 +59,18 @@ Bacteria [] colony;
    // 		bactY = bactY + (int)(Math.random()*2)+20;
    // }
   }
-  void show(){
+  public void show(){
   	noStroke();
     fill(bactColor,bactColor2, bactColor3);
     ellipse(bactX, bactY, 20,20);
   }
  }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
